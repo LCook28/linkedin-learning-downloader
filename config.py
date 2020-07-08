2d96c6792f63940f4ff3614690e98b97e9a8cac5
LCook28 <LCook28@users.noreply.github.com>
2020-07-08 19:47:49 +0500

    Update config.py
    Added Toggle for Exercise Files & Description

--- config.py
+++ config.py
@@ -3,7 +3,11 @@
 USERNAME = ''
 PASSWORD = ''
 DEFAULT_DOWNLOAD_PATH = '' #use "/" as separators
+DESC = True
+EX_FILES = True
 SUBS = True
+#START_TIME = 00:00
+#END_TIME = 06:55
 COURSES = [
     'it-security-foundations-core-concepts',
     'charlene-li-on-digital-leadership',
