daf47668171e0b9cc310b9b3eb5ac681c3e78b7a
LCook28 <LCook28@users.noreply.github.com>
2020-07-10 02:31:22 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ DEFAULT_DOWNLOAD_PATH = '' #use "/" as separators
 DESC = True
 EX_FILES = True
 SUBS = True
+LIMIT_DOWNLOAD_TIMES = False
+START_TIME = '00:00' #use 24 hour time
+END_TIME = '23:59' #use 24 hour time
 COURSES = [
     'it-security-foundations-core-concepts',
     'charlene-li-on-digital-leadership',
